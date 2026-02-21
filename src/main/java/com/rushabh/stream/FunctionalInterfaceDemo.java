be7d6d7d9299e3a90639aebc3f5afecd71547bfa
Rushabh Chalikwar <rushabhchalikwar@gmail.com>
2026-02-21 11:41:00 +0530

    Updated FunctionalInterfaceDemo

--- src/main/java/com/rushabh/stream/FunctionalInterfaceDemo.java
+++ src/main/java/com/rushabh/stream/FunctionalInterfaceDemo.java
@@ -6,13 +6,51 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+/**
+ * Custom functional interface for demonstrating lambda expressions.
+ * This interface takes a String parameter and performs an operation.
+ */
 @FunctionalInterface
 interface A {
+    /**
+     * Displays a message based on the provided name.
+     *
+     * @param name the name to be displayed
+     */
     void show(String name);
 }
 
-
+/**
+ * Demonstration class showcasing built-in functional interfaces from java.util.function package.
+ * <p>
+ * This class provides practical examples of the four main functional interfaces:
+ * <ul>
+ *   <li>{@link Predicate} - Tests a condition and returns a boolean</li>
+ *   <li>{@link Function} - Transforms input to output</li>
+ *   <li>{@link Consumer} - Performs an action on input without returning a value</li>
+ *   <li>{@link Supplier} - Provides/generates a value</li>
+ * </ul>
+ * </p>
+ *
+ * @author Rushabh
+ * @version 1.0
+ */
 public class FunctionalInterfaceDemo{
+    /**
+     * Main method demonstrating the usage of various functional interfaces.
+     * <p>
+     * Examples included:
+     * <ol>
+     *   <li>Custom functional interface A - demonstrates lambda with String parameter</li>
+     *   <li>Predicate - tests if a string is empty</li>
+     *   <li>Function - returns the length of a string</li>
+     *   <li>Consumer - prints a string to console</li>
+     *   <li>Supplier - generates a random double value</li>
+     * </ol>
+     * </p>
+     *
+     * @param args command line arguments (not used)
+     */
     public static void main(String[] args) {
         A a = (name) -> System.out.println("Hello from functional interface! " + name);
         a.show("Rushabh");
@@ -28,7 +66,5 @@ public class FunctionalInterfaceDemo{
 
         Supplier<Double> random = () -> Math.random();
         random.get();            // 0.735...
-
-        List
     }
 }
